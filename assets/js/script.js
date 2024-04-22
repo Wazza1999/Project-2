cfeb6893227ca7271ca7a70cf85a0cef1bfe8cb8
Wazza1999 <warren.selby@hotmail.co.uk>
2024-04-22 09:55:49 +0000

    finally got modal working for the how to play section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+const modalContainer = document.getElementById("modal-container");
 
+openModal.addEventListener("click",() => {
+    modalContainer.classList.add("show");
+});
+
+closeModal.addEventListener("click",() => {
+modalContainer.classList.remove("show");
+});
