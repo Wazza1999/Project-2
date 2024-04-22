1f709b800691a5e7e6fdfdf2685da1f1e4f773c0
Wazza1999 <warren.selby@hotmail.co.uk>
2024-04-22 09:57:31 +0000

    added API to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+let data = {};
+let questionNo = 0;
+
+
+// API
+const easyQuiz = "https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple";
+const mediumQuiz ="https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple";
+const hardQuiz ="https://opentdb.com/api.php?amount=5&category=15&difficulty=hard&type=multiple";
+
